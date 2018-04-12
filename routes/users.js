9d4cad0dae4272db1b78dba6e773ff34fa0071ff
Michelle Schapmire <mschap09@gmail.com>
2018-04-11 20:10:20 -0500

    added login route, JWTs, and authorization middleware

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,42 @@
 
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
 var userController = require('../controllers/user');
 var models = require('../models');
+var config = require('../config/config');
+var VerifyToken = require('./VerifyToken');
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+// GET user info minus password
+router.get('/me', VerifyToken, function(req, res, next) {      
+  userController.getUserById(decoded.id)
+  .then((user) => {
+    if (!user) return res.status(404).send("No user found.");
+    user.password = "0";
+    res.status(200).send(user);
+  }, (err) => {
+    if (err) return res.status(500).send("There was a problem finding the user.");
+  });
+});
+
+// middleware function
+router.use(function (user, req, res, next) {
+  res.status(200).send(user);
+});
+
 /* POST new user. */
-router.post('/', function(req, res, next) {
+router.post('/register', function(req, res, next) {
   let newUser = models.User.build({      // create a new instance of the User model
     firstName: req.body.firstName,  // set the user name... (comes from the request)
     lastName: req.body.lastName,
@@ -18,7 +44,41 @@ router.post('/', function(req, res, next) {
     password: req.body.password
   });
 
-  userController.addUser(newUser);
+  userController.addUser(newUser)
+  .then((user) => {
+      // create a token
+      console.log(user.id);
+      var token = jwt.sign({ id: user.id }, config.secret, {
+        expiresIn: 86400 // expires in 24 hours
+      });
+
+      res.status(200).send({ auth: true, token: token });
+  }, (err) => {
+    if (err) return res.status(500).send("There was a problem registering the user.");
+  });
 });
+
+
+// POST login
+router.post('/login', function(req, res) {
+  userController.getUserByEmail(req.body.email)
+  .then((user) => {
+    if (!user) return res.status(404).send('No user found.');
+
+    var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+    if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
+
+    var token = jwt.sign({ id: user.id }, config.secret, {
+      expiresIn: 86400 // expires in 24 hours
+    });
+
+    res.status(200).send({ auth: true, token: token });
+  }, (err) => {
+    if (err) return res.status(500).send('Error on the server.');
+  });
+});
+
+
+
 
 module.exports = router;
